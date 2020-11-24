f56858830b460cf2c96ca2118d5ebfce7db72df7
Azahar Hossain <azahar.hossain10@yahoo.com>
2020-11-24 13:42:23 +0100

    Logging Service and account service injection

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,18 +1,18 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component} from '@angular/core';
+import { AccountsService } from '../services/accounts.service';
+import { LoggingService } from '../services/logging.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers:[LoggingService]
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+  constructor(private loggingService:LoggingService, private acccountsService: AccountsService){}
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-    console.log('A server status changed, new status: ' + accountStatus);
+    this.acccountsService.addAccount(accountName,accountStatus);
+    this.loggingService.logStatusChanged(accountStatus);
   }
 }
